748b101418abe9566f719a9d1022042c6788155f
Abhishek Kumar <abhishek160206@gmail.com>
2026-02-07 13:21:29 +0530

    Initalization and Declaration

---
+++ Basic_variables.java
@@ -0,0 +1,10 @@
+public class Basic_variables {
+    public static void main(String[] args) {
+        int x = 10; // Declare and initialize variable x
+        System.out.println("The value of x is: " + x);
+
+        int y; // Declare variable y without initializing
+        y = 20; // Initialize variable y after declaration
+        System.out.println("The value of y is: " + y);
+    }
+}
