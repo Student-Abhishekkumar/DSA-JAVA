ddf36331e7bb866cdef636749c545f865459996f
Abhishek Kumar <abhishek160206@gmail.com>
2026-02-08 23:59:15 +0530

    Calculations

---
+++ Basics/areaofcircle.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class areaofcircle {
+    public static void main(String[] args) {
+        double r = 5;
+        double a = 3.14 * r * r ;
+        System.out.println("Area of Circle : " + a );
+
+    }
+}
