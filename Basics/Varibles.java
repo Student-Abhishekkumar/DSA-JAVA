045ff9c54e52f6331e0ccd565bb7815d70457612
Abhishek Kumar <abhishek160206@gmail.com>
2026-03-24 11:56:38 +0530

    varibles

--- Basics/Varibles.java
+++ Basics/Varibles.java
@@ -2,8 +2,8 @@ package Basics;
 
 public class Varibles {
     public static void main(String[] args) {
-        int x = 10; // Declare and initialize variable x
-        System.out.println("The value of x is: " + x);
+        int _x = 10; // Declare and initialize variable x
+        System.out.println("The value of x is: " + _x);
 
         int y; // Declare variable y without initializing
         y = 20; // Initialize variable y after declaration
