a439c25257ffded3817be06bddfca7772b9b19a7
Abhishek Kumar <abhishek160206@gmail.com>
2026-07-25 22:14:34 +0530

    Practice

---
+++ Patterns/pattern5.java
@@ -0,0 +1,13 @@
+public class pattern5 {
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+        System.out.println();
+        }
+    }
+}
